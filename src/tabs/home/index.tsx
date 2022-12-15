5df6dcaf45deb1e9e426ab2f2798f09004c63c0e
PlatzerT <thpl.school@gmail.com>
2022-12-15 13:39:00 +0100

    .

--- src/tabs/home/index.tsx
+++ src/tabs/home/index.tsx
@@ -1,4 +1,3 @@
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import HomeScreen from "../../screens/home";
